a1dc256d8b5b6d2dfd96926087511233ae89e476
zacharydscott <zachary.douglas.scott@gmail.com>
2018-09-20 19:39:35 -0400

    added a remove exercise button to post workout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import * as React from "react";
 import "./App.css";
-import './include/mdb';
+import "./include/mdb";
 // import RegisterUser from "./components/register-user";
 // import NewWorkout from "./components/new-workout";
 // import ViewWorkout from "./components/view-workout";
 import Login from "./components/login";
 import Dashboard from "./components/dashboard/dashboard";
 import ViewWorkoutHistory from "./components/view-workout-history";
+import NewWorkout from "./components/new-workout";
 import { store } from "./store";
 import { Provider } from "react-redux";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
-import {HomeComponent} from "./components/home/home.component";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HomeComponent } from "./components/home/home.component";
 
 class App extends React.Component {
   public render() {
@@ -18,15 +19,19 @@ class App extends React.Component {
       <Provider store={store}>
         <BrowserRouter>
           <div>
-              <div id="main-content-container">
-                  <Switch>
-                      <Route path="/home" component={HomeComponent} />
-                    <Route path="/dashboard" component={Dashboard} />
-                      <Route path="/view-workout-history" component={ViewWorkoutHistory}/>
-                      <Route path="/login" component={Login} />
-                      <Route component={HomeComponent} />
-                  </Switch>
-              </div>
+            <div id="main-content-container">
+              <Switch>
+                <Route path="/home" component={HomeComponent} />
+                <Route path="/dashboard" component={Dashboard} />
+                <Route path="/post-workout" component={NewWorkout} />
+                <Route
+                  path="/view-workout-history"
+                  component={ViewWorkoutHistory}
+                />
+                <Route path="/login" component={Login} />
+                <Route component={HomeComponent} />
+              </Switch>
+            </div>
           </div>
         </BrowserRouter>
       </Provider>
