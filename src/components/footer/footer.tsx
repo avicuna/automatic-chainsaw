10ea58a2a3d76592a7cbaf2a9a71eeb2ab4ab821
alexvicuna <vicunaalejandro@gmail.com>
2018-09-23 17:01:03 -0400

    - Edited home page to be more presentable for the presentation

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
-import { Container, Footer} from 'mdbreact';
+import { Container, } from 'mdbreact';
 
 class FooterPage extends React.Component {
     public render(){
         return(
-            <Footer color="blue" className="font-small pt-4 mt-4">
                 <div className="footer-copyright text-center py-3">
                     <Container fluid>
                         &copy; {(new Date().getFullYear())} Copyright: GainTrain
                     </Container>
                 </div>
-            </Footer>
         );
     }
 }
